919a766d08791b266a26a10745ff7e26f5bf9a47
florentplomb <plombf@gmail.com>
2016-08-14 00:10:57 +0200

    fixe html upload

--- client/app/constants.js
+++ client/app/constants.js
@@ -1,3 +1,3 @@
 angular.module('travelMapApp.constants')
-  .constant('apiUrl', 'http://hawaii2016.herokuapp.com/api');
-//  .constant('apiUrl', 'http://localhost:9000/api');
+ // .constant('apiUrl', 'http://hawaii2016.herokuapp.com/api');
+ .constant('apiUrl', 'http://localhost:9000/api');
